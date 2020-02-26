14c6d1f6b36b672656ddfbd9f5952734651e841a
Frederic Triplett, Jr <frederic1.triplett@famu.edu>
2020-02-26 05:14:30 -0500

    Initial commit - Start of Part 4 tutorial - Created Login view and added on click listener to the login button

---
+++ app/src/main/java/com/example/parstagram/LoginActivity.java
@@ -0,0 +1,41 @@
+package com.example.parstagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginActivity";
+
+    EditText etUsername;
+    EditText etPassword;
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        // Button OnClickListener Time
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                Toast.makeText(LoginActivity.this, "Button pressed", Toast.LENGTH_SHORT).show();
+                Log.i(TAG, "Username: " + username + "   ---    Password entered");
+            }
+        });
+    }
+}
