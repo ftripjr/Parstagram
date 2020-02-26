881c9e3bc38dd96672123948dd1d5f46d4393857
Frederic Triplett, Jr <frederic1.triplett@famu.edu>
2020-02-26 06:41:15 -0500

    Finished Video 4 of 6 for tutorial. Successfully logged in and removed LoginActivity from the stack if user previously logged in.

--- app/src/main/java/com/example/parstagram/LoginActivity.java
+++ app/src/main/java/com/example/parstagram/LoginActivity.java
@@ -2,6 +2,8 @@ package com.example.parstagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,6 +11,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -23,6 +28,9 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        if(ParseUser.getCurrentUser() != null)
+            goMainActivity();
+
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
@@ -33,9 +41,32 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String username = etUsername.getText().toString();
                 String password = etPassword.getText().toString();
-                Toast.makeText(LoginActivity.this, "Button pressed", Toast.LENGTH_SHORT).show();
-                Log.i(TAG, "Username: " + username + "   ---    Password entered");
+                loginUser(username, password);
             }
         });
+    }
+
+    private void loginUser(String username, String password) {
+        /* Note: logInInBackground is preferred over logIn because
+        using this works on the background thread instead of on the main thread,
+        which allows for a better user experience by letting them use other apps while the app logins
+         on a different thread*/
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e != null) {
+                    Log.e(TAG, "Error logging in", e);
+                    return;
+                }
+                goMainActivity();
+                Toast.makeText(LoginActivity.this, "Login Succesful!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
---
+++ app/src/main/java/com/example/parstagram/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.parstagram;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
