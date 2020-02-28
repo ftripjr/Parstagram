d73d2ab3b4904bc974e1598be0231c976e68d2d3
Frederic Triplett, Jr <frederic1.triplett@famu.edu>
2020-02-28 04:20:28 -0500

    Finished Video 5 of 6 for tutorial.
    Post queried in the logcat.
    Able to query posts from Parse Database

--- app/src/main/java/com/example/parstagram/MainActivity.java
+++ app/src/main/java/com/example/parstagram/MainActivity.java
@@ -1,18 +1,59 @@
 package com.example.parstagram;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
+
+    private EditText etCaption;
+    private Button btnCapture;
+    private Button btnSubmit;
+    private ImageView ivPostImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        etCaption = findViewById(R.id.etPassword);
+        btnCapture = findViewById(R.id.btnCapture);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        ivPostImage = findViewById(R.id.ivPostImage);
+
+        queryPosts();
+    }
+
+    private void queryPosts() {
+        // What class do I want to query? Posts!
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        // Now to find the ID(s)
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if(e != null) {
+                    Log.e(TAG, "I can't reach the posts!", e);
+                    return;
+                }
+                for (Post post : posts) {
+                    Log.i(TAG, "Post: " + post.getCaption() + ", and the user is: " + post.getUser().getUsername());
+
+                }
+            }
+        });
     }
 }
